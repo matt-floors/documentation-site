a8ca6c041ab3b7a3d8f335923b496aced47b01a2
matt-floors <matteo.piani@e-matica.it>
2025-06-05 10:12:24 +0200

    update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: 'https://localhost:3000',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/ew_library_docs/',
+  baseUrl: '/', // baseUrl: '/ew_library_docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
